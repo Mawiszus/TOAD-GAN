76dece8f477f6520e65471799412b7dec175bb92
Frederik Schubert <frederik.schubert@mailbox.org>
2021-03-24 13:37:36 +0100

    stub for generating directional noise

--- generate_noise.py
+++ generate_noise.py
@@ -2,9 +2,16 @@
 import torch
 
 
-def generate_spatial_noise(size, device, *args, **kwargs):
+def generate_spatial_noise(size, device, embeddings=None, *args, **kwargs):
     """ Generates a noise tensor. Currently uses torch.randn. """
     # noise = generate_noise([size[0], *size[2:]], *args, **kwargs)
     # return noise.expand(size)
-    return torch.randn(size, device=device)
+    if embeddings is not None:
+        directions = torch.stack(embeddings.values())
+        rand_directions = torch.randint(0, len(directions), size[0], *size[2:])
+        
+        # TODO(frederik): return randn amount in direction from current point to chosen embedding
+        pass
+    else:
+        return torch.randn(size, device=device)
 
